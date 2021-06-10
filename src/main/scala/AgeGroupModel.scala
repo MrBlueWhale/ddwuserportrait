f549bd9ff22e6eb983fae058b343d79fc88fda69
zengdaxia <1023479515@qq.com>
2021-06-10 09:34:45 +0800

    z-1-3 完成年龄段标签开发

---
+++ src/main/scala/AgeGroupModel.scala
@@ -0,0 +1,63 @@
+import org.apache.spark.sql.{DataFrame, Row, SparkSession}
+import org.apache.spark.sql.execution.datasources.hbase.HBaseTableCatalog
+import org.apache.spark.sql.functions._
+
+
+
+  object AgeGroupModel {
+
+    def main(args: Array[String]): Unit = {
+      def catalog =
+        s"""{
+           |"table":{"namespace":"default", "name":"tbl_users"},
+           |"rowkey":"id",
+           |"columns":{
+           |"id":{"cf":"rowkey", "col":"id", "type":"string"},
+           |"ageGroup":{"cf":"cf", "col":"birthday", "type":"string"}
+           |}
+           |}""".stripMargin
+
+      val spark = SparkSession.builder()
+        .appName("shc test")
+        .master("local[10]")
+        .getOrCreate()
+
+      import spark.implicits._
+
+      val readDF: DataFrame = spark.read
+        .option(HBaseTableCatalog.tableCatalog, catalog)
+        .format("org.apache.spark.sql.execution.datasources.hbase")
+        .load()
+
+      val birth2AgeGroup = (date: String) => {
+        val year = date.charAt(2)
+        s"${year}0"
+      }
+      spark.udf.register("birth2AgeGroup",birth2AgeGroup)
+
+      readDF.createTempView("tb")
+
+      val result = spark.sql("select id, birth2AgeGroup(ageGroup) as ageGroup from tb").toDF()
+
+
+      result.show()
+
+      def catalogWrite =
+        s"""{
+           |"table":{"namespace":"default", "name":"user_profile"},
+           |"rowkey":"id",
+           |"columns":{
+           |"id":{"cf":"rowkey", "col":"id", "type":"string"},
+           |"ageGroup":{"cf":"cf", "col":"ageGroup", "type":"string"}
+           |}
+           |}""".stripMargin
+
+      result.write
+        .option(HBaseTableCatalog.tableCatalog, catalogWrite)
+        .option(HBaseTableCatalog.newTable, "5")
+        .format("org.apache.spark.sql.execution.datasources.hbase")
+        .save()
+    }
+  }
+
+
