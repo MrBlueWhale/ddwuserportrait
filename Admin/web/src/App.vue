ff842e13a79d26e7c3be25c0b1d15298d04c8a29
MrBlueWhale <2971529737@qq.com>
2021-06-11 14:14:27 +0800

    lx-4-1 前端框架搭建完成

---
+++ Admin/web/src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <a-layout>
+<admin-header></admin-header>
+    <router-view></router-view>
+    <a-layout-footer style="text-align: center">
+      Dalao Template @2021
+    </a-layout-footer>
+  </a-layout>
+</template>
+
+<script lang="ts">
+import {defineComponent} from 'vue';
+import AdminHeader from '@/components/admin-header.vue';
+
+export default defineComponent({
+  name: 'app',
+  components: {
+    AdminHeader,
+  },
+
+});
+</script>
+
+
+
+
+<style>
+#components-layout-demo-top-side-2 .logo {
+  float: left;
+  width: 120px;
+  height: 31px;
+  margin: 16px 24px 16px 0;
+  background: rgba(255, 255, 255, 0.3);
+}
+
+.ant-row-rtl #components-layout-demo-top-side-2 .logo {
+  float: right;
+  margin: 16px 0 16px 24px;
+}
+
+.site-layout-background {
+  background: #fff;
+}
+</style>
