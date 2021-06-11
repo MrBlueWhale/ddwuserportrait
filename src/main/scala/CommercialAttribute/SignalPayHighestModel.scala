95c93b5b74a583a5231a6a6c4c387e0f2b2e86c6
zzc <25124661332qq.com>
2021-06-11 10:26:37 +0800

    新增标签单笔最高

---
+++ src/main/scala/CommercialAttribute/SignalPayHighestModel.scala
@@ -0,0 +1,54 @@
+package CommercialAttribute
+
+import org.apache.spark.sql.execution.datasources.hbase.HBaseTableCatalog
+import org.apache.spark.sql.functions._
+import org.apache.spark.sql.{DataFrame, SparkSession}
+
+object SignalPayHighestModel {
+  def main(args: Array[String]): Unit = {
+    def catalog =
+      s"""{
+         |  "table":{"namespace":"default", "name":"tbl_orders"},
+         |  "rowkey":"id",
+         |  "columns":{
+         |    "id":{"cf":"rowkey", "col":"id", "type":"string"},
+         |    "memberId":{"cf":"cf", "col":"memberId", "type":"string"},
+         |    "paidAmount":{"cf":"cf", "col":"paidAmount", "type":"string"}
+         |  }
+         |}""".stripMargin
+
+    val spark = SparkSession.builder()
+      .appName("shc test")
+      .master("local[10]")
+      .getOrCreate()
+
+    import spark.implicits._
+    val source: DataFrame = spark.read
+      .option(HBaseTableCatalog.tableCatalog, catalog)
+      .format("org.apache.spark.sql.execution.datasources.hbase")
+      .load()
+
+    source.show(20, false)
+
+    val result = source.groupBy('memberId as "id")
+      .agg(max('paidAmount) as "highestPay")
+
+    result.show(20, false)
+
+    def catalogWrite =
+      s"""{
+         |"table":{"namespace":"default", "name":"user_profile"},
+         |"rowkey":"id",
+         |"columns":{
+         |  "id":{"cf":"rowkey", "col":"id", "type":"string"},
+         |  "highestPay":{"cf":"cf", "col":"highestPay", "type":"string"}
+         |}
+         |}""".stripMargin
+
+    result.write
+      .option(HBaseTableCatalog.tableCatalog, catalogWrite)
+      .option(HBaseTableCatalog.newTable, "5")
+      .format("org.apache.spark.sql.execution.datasources.hbase")
+      .save()
+  }
+}
