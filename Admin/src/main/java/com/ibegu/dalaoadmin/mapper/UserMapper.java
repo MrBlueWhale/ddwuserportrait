dbc71cc4077acb1cf38e6775d47f5c96a757a68c
MrBlueWhale <2971529737@qq.com>
2021-06-11 15:11:04 +0800

    lx-7-1 生成user表持久层代码

---
+++ Admin/src/main/java/com/ibegu/dalaoadmin/mapper/UserMapper.java
@@ -0,0 +1,30 @@
+package com.ibegu.dalaoadmin.mapper;
+
+import com.ibegu.dalaoadmin.domain.User;
+import com.ibegu.dalaoadmin.domain.UserExample;
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+
+public interface UserMapper {
+    long countByExample(UserExample example);
+
+    int deleteByExample(UserExample example);
+
+    int deleteByPrimaryKey(Long uid);
+
+    int insert(User record);
+
+    int insertSelective(User record);
+
+    List<User> selectByExample(UserExample example);
+
+    User selectByPrimaryKey(Long uid);
+
+    int updateByExampleSelective(@Param("record") User record, @Param("example") UserExample example);
+
+    int updateByExample(@Param("record") User record, @Param("example") UserExample example);
+
+    int updateByPrimaryKeySelective(User record);
+
+    int updateByPrimaryKey(User record);
+}
