31e5f2ffda880e7c12cc4d8f97d51b037b118cef
MrBlueWhale <15775837993@163.com>
2021-06-12 15:14:09 +0800

    lx-7-7 前端增加密码加盐加密 在加密页面内使用： declare let hexMd5: any; declare let KEY: any;
    user.value.password = hexMd5(user.value.password + KEY);

--- Admin/src/main/java/com/ibegu/dalaoadmin/controller/UserController.java
+++ Admin/src/main/java/com/ibegu/dalaoadmin/controller/UserController.java
@@ -1,11 +1,13 @@
 package com.ibegu.dalaoadmin.controller;
 
 import com.alibaba.fastjson.JSONObject;
+import com.ibegu.dalaoadmin.req.UserLoginReq;
 import com.ibegu.dalaoadmin.req.UserQueryReq;
 import com.ibegu.dalaoadmin.req.UserResetPasswordReq;
 import com.ibegu.dalaoadmin.req.UserSaveReq;
 import com.ibegu.dalaoadmin.resp.CommonResp;
 import com.ibegu.dalaoadmin.resp.PageResp;
+import com.ibegu.dalaoadmin.resp.UserLoginResp;
 import com.ibegu.dalaoadmin.resp.UserQueryResp;
 import com.ibegu.dalaoadmin.service.UserService;
 import com.ibegu.dalaoadmin.utils.SnowFlake;
@@ -70,20 +72,20 @@ public class UserController {
         return resp;
     }
     //
-    // @PostMapping("/login")
-    // public CommonResp login(@Valid @RequestBody UserLoginReq req) {
-    //     req.setPassword(DigestUtils.md5DigestAsHex(req.getPassword().getBytes()));
-    //     CommonResp<UserLoginResp> resp = new CommonResp<>();
-    //     UserLoginResp userLoginResp = userService.login(req);
-    //
-    //     Long token = snowFlake.nextId();
-    //     LOG.info("生成单点登录token：{}，并放入redis中", token);
-    //     userLoginResp.setToken(token.toString());
-    //     redisTemplate.opsForValue().set(token.toString(), JSONObject.toJSONString(userLoginResp), 3600 * 24, TimeUnit.SECONDS);
-    //
-    //     resp.setContent(userLoginResp);
-    //     return resp;
-    // }
+    @PostMapping("/login")
+    public CommonResp login(@Valid @RequestBody UserLoginReq req) {
+        req.setPassword(DigestUtils.md5DigestAsHex(req.getPassword().getBytes()));
+        CommonResp<UserLoginResp> resp = new CommonResp<>();
+        UserLoginResp userLoginResp = userService.login(req);
+
+        // Long token = snowFlake.nextId();
+        // LOG.info("生成单点登录token：{}，并放入redis中", token);
+        // userLoginResp.setToken(token.toString());
+        // redisTemplate.opsForValue().set(token.toString(), JSONObject.toJSONString(userLoginResp), 3600 * 24, TimeUnit.SECONDS);
+
+        resp.setContent(userLoginResp);
+        return resp;
+    }
     //
     // @GetMapping("/logout/{token}")
     // public CommonResp logout(@PathVariable String token) {
