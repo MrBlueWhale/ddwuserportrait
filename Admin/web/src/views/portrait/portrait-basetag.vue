a8ced3c413daa6d7ef0dd915151a43cbcaf3a5cb
MrBlueWhale <2971529737@qq.com>
2021-06-14 17:31:25 +0800

    lx-11-5 集成使用词云 暂时取消后端登录拦截器（后端接口不知道咋加上header，便于调试http接口）

--- Admin/web/src/views/portrait/portrait-basetag.vue
+++ Admin/web/src/views/portrait/portrait-basetag.vue
@@ -21,6 +21,12 @@
       </a-col>
     </a-row>
 
+    <a-row>
+      <a-col :span="24" id="main-col">
+        <div id="wordCloud" style="width:100%;height:400px;"></div>
+      </a-col>
+    </a-row>
+
   </a-layout>
 </template>
 
@@ -28,12 +34,17 @@
 <script lang="ts">
 import {defineComponent, onMounted, ref, reactive, toRef} from 'vue';
 
+// import resizefrom "./mixins/resize";
+
+// import"echarts-wordcloud/dist/echarts-wordcloud";
+//
+// import"echarts-wordcloud/dist/echarts-wordcloud.min";
+
 import axios from 'axios';
 
 declare let echarts: any;
 
 // import HelloWorld from "@/components/HelloWorld.vue";
-
 
 
 
@@ -150,7 +161,7 @@ export default defineComponent({
      ];
 
       var option = {
-  backgroundColor: '#8076a3',
+  backgroundColor: '#525288',
   title: [
     {
       text: '各渠道投诉占比',
@@ -261,6 +272,149 @@ export default defineComponent({
 
    };
 
+    const testWordCloud = () => {
+      // 基于准备好的dom，初始化echarts实例
+      const myWordCloud = echarts.init(document.getElementById('wordCloud'));
+
+      // 指定图表的配置项和数据
+      const option ={
+        title: {
+          text: '词云',
+          x:"center"
+        },
+
+        backgroundColor:"#c6fbff",//tooltip: {//pointFormat: "{series.name}: {point.percentage:.1f}%"//},
+        series: [
+          {
+            type:"wordCloud",//用来调整词之间的距离
+            gridSize: 10,//用来调整字的大小范围//Text size range which the value in data will be mapped to.//Default to have minimum 12px and maximum 60px size.
+            sizeRange: [14, 60],//Text rotation range and step in degree. Text will be rotated randomly in range [-90, 90] by rotationStep 45//用来调整词的旋转方向，，[0,0]--表明着没有角度，也就是词为水平方向，须要设置角度参考注释内容//rotationRange: [-45, 0, 45, 90],//rotationRange: [ 0,90],
+            rotationRange: [0, 0],//随机生成字体颜色//maskImage: maskImage,
+            textStyle: {
+              // normal: {
+              //   color: function() {return("rgb(" +Math.round(Math.random()* 255) +
+              //
+              //       "," +Math.round(Math.random()* 255) +
+              //
+              //       "," +Math.round(Math.random()* 255) +
+              //
+              //       ")");
+              //
+              //   }
+              // }
+            },
+            //位置相关设置//Folllowing left/top/width/height/right/bottom are used for positioning the word cloud//Default to be put in the center and has 75% x 80% size.
+            left: "center",
+            top:"center",
+            right:null,
+            bottom:null,
+            width:"200%",
+            height:"200%",//数据
+            data: [
+              {
+                name: "十九大精神",
+                value: 15000
+              },
+              {
+                name: "两学一作",
+                value: 10081
+              },
+              {
+                name: "中华民族",
+                value: 9386
+              },
+              {
+                name: "马克思主义",
+                value: 7500
+              },
+              {
+                name: "民族复兴",
+                value: 7500
+              },
+              {
+                name: "社会主义制度",
+                value: 6500
+              },
+              {
+                name: "国防白皮书",
+                value: 6500
+              },
+              {
+                name: "创新",
+                value: 6000
+              },
+              {
+                name: "民主革命",
+                value: 4500
+              },
+              {
+                name: "文化强国",
+                value: 3800
+              },
+              {
+                name: "国家主权",
+                value: 3000
+              },
+              {
+                name: "武装颠覆",
+                value: 2500
+              },
+              {
+                name: "领土完整",
+                value: 2300
+              },
+              {
+                name: "安全",
+                value: 2000
+              },
+              {
+                name: "从严治党",
+                value: 1900
+              },
+              {
+                name: "现代化经济体系",
+                value: 1800
+              },
+              {
+                name: "国防动员",
+                value: 1700
+              },
+              {
+                name: "信息化战争",
+                value: 1600
+              },
+              {
+                name: "局部战争",
+                value: 1500
+              },
+              {
+                name: "教育",
+                value: 1200
+              },
+              {
+                name: "职业教育",
+                value: 1100
+              },
+              {
+                name: "兵法",
+                value: 900
+              },
+              {
+                name: "一国两制",
+                value: 800
+              },
+              {
+                name: "特点社会主义思想",
+                value: 700
+              },
+            ]
+          }
+        ]
+      };
+      // 使用刚指定的配置项和数据显示图表。
+      myWordCloud.setOption(option);
+    };
+
 
     //初始化逻辑都写到onMounted()里
     onMounted(() => {
@@ -269,6 +423,7 @@ export default defineComponent({
       testEcharts();
       testCalendarChart();
       testPieChart();
+      testWordCloud();
 
 
     });
